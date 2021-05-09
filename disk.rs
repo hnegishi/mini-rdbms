7853c50f473a7f17c3579bc78f0f63af757bbb4f
hnegishi <hnegishimaru@gmail.com>
2021-05-09 22:34:24 +0900

    [add] disk methods

--- disk.rs
+++ disk.rs
@@ -5,3 +5,25 @@ pub struct DiskManager {
   // 裁判するページIDを決めるカウンタ
   next_page_id: u64,
 }
+
+impl DiskManager {
+  // constracter
+  pub fn new(date_file: File) -> io::Result<Self> {
+
+  }
+}
+
+// open file path
+pub fn open(data_file_path: impl AsRef<Path>) -> io::Result<Self> {
+
+}
+
+// read page data
+pub fn read_page_data(&mul self, page_id: PageId, data: mut [u8]) -> io::Result<()> {
+
+}
+
+// write page data
+pub fn write_page_data(&mut self, page_id: PageId, data: &[u8]) -> io::Result<()> {
+
+}

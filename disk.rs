a79d15284204e3302e75eb2b681b0eb1316a7642
hnegishi <hnegishimaru@gmail.com>
2021-05-09 22:58:26 +0900

    [add] DiskManager::new

--- disk.rs
+++ disk.rs
@@ -13,28 +13,35 @@ pub struct DiskManager {
 impl DiskManager {
   // constracter
   pub fn new(date_file: File) -> io::Result<Self> {
+    // ファイルサイズ取得
+    let heap_file_size = heap_file.metadata()?.len();
+    // 次に裁判するブロック単位のIDを計算
+    let next_page_id = heap_file_size / PAGE_SIZE as u64;
+    Ok(Self{
+      heap_file,
+      next_page_id,
+    })
+  }
+
+  // open heap file
+  //pathを指定して開く
+  pub fn open(data_file_path: impl AsRef<Path>) -> io::Result<Self> {
+    let heap_file = OpenOptions::new()
+      .read(true)
+      .write(true)
+      .create(true)
+      .open(heap_file_path)?; // ?はエラーが返った時の早期return。
+    Self::new(heap_file) // DiskManager::open -self-> DiskManager::new
+  }
+
+  // read page data
+  // &self &mut self はレシーバ
+  pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
 
   }
-}
 
-// open heap file
-//pathを指定して開く
-pub fn open(data_file_path: impl AsRef<Path>) -> io::Result<Self> {
-  let heap_file = OpenOptions::new()
-    .read(true)
-    .write(true)
-    .create(true)
-    .open(heap_file_path)?; // ?はエラーが返った時の早期return。
-  Self::new(heap_file) // DiskManager::open -self-> DiskManager::new
-}
+  // write page data
+  pub fn write_page_data(&mut self, page_id: PageId, data: &[u8]) -> io::Result<()> {
 
-// read page data
-// &self &mut self はレシーバ
-pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
-
-}
-
-// write page data
-pub fn write_page_data(&mut self, page_id: PageId, data: &[u8]) -> io::Result<()> {
-
+  }
 }
